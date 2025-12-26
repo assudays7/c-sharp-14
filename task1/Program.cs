b2bd5a4c774bd69559c83ae545dfbbaa7883c234
Sudays <assudays571@outlook.com>
2025-12-26 16:58:10 +0500

    addd

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using task1;
+
+System.Console.WriteLine("Student:");
+
+Student student = new Student("Muhammadjon", "Dushanbe", 3, ["Dart", "Frontend", "C#"], [83, 81, 75]);
+student.AddCourseGrade(".Net", 80);
+
+double average = student.GetAverageGrade();
+System.Console.WriteLine(average);
+
+student.PrintGrades();
+System.Console.WriteLine("Teacher:");
+
+Teacher teacher = new Teacher("Alijon", "Dushanbe", 2, [".NET", "C#"]);
+
+teacher.AddCourse("Git");
+
+teacher.AddCourse("C++");
+
+teacher.RemoveCourse("Git");
+
+teacher.PrintGrades();
